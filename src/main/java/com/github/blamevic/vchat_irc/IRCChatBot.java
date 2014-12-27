0da648bef169a599dc9088e8eed9a0df1fd64a12
RX14 <chris@rx14.co.uk>
2014-12-27 23:11:40 +0000

    Mah butt does all the things!

--- src/main/java/com/github/blamevic/vchat_irc/IRCChatBot.java
+++ src/main/java/com/github/blamevic/vchat_irc/IRCChatBot.java
@@ -1,25 +1,33 @@
 package com.github.blamevic.vchat_irc;
 
 import com.github.blamevic.irc.IRCClient;
+import com.github.blamevic.irc.IRCMessageParser;
 import org.yaml.snakeyaml.Yaml;
-import vic.mod.chat.api.bot.IBotHandler;
-import vic.mod.chat.api.bot.IChannelBase;
-import vic.mod.chat.api.bot.IChatBot;
-import vic.mod.chat.api.bot.IChatEntity;
+import vic.mod.chat.api.bot.*;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Map;
-import java.util.Objects;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
+@Version(version = "1.3")
 public class IRCChatBot implements IChatBot {
     IRCClient irc;
     IBotHandler handler;
 
+    String channel;
+
+    BlockingQueue<String> ircQueue;
+    BlockingQueue<String> minecraftChatQueue;
+
     @Override
     public void onLoad(IBotHandler handler) {
         this.handler = handler;
+        this.ircQueue = new LinkedBlockingQueue<>();
+        this.minecraftChatQueue = new LinkedBlockingQueue<>();
 
         File configFile = new File(handler.getBotDir() + "/irc.yml");
 
@@ -27,21 +35,59 @@ public class IRCChatBot implements IChatBot {
         try {
             conf = (Map<String, Object>) new Yaml().load(new FileInputStream(configFile));
         } catch (FileNotFoundException e) {
-            System.err.println("Could not find file" + configFile.getPath());
+            System.err.println("Could not find file " + configFile.getAbsolutePath());
             throw new RuntimeException("VChat api sux");
         }
 
         String hostname = (String) conf.get("hostname");
-        int port = (int) conf.get("port");
+        int port = (Integer) conf.get("port");
         String username = (String) conf.get("username");
         String realname = (String) conf.get("realname");
         boolean debug;
         if (conf.get("debug") != null)
-            debug = (boolean) conf.get("debug");
+            debug = (Boolean) conf.get("debug");
         else
             debug = false;
 
+        this.channel = (String) conf.get("channel");
+
         this.irc = new IRCClient(hostname, port, username, realname, debug);
+
+        try {
+            irc.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("VChat api sux");
+        }
+        irc.login();
+        irc.waitForCommand("001");
+        irc.joinChannel(channel);
+
+        new Thread(() -> {
+            while (true) {
+                String message = null;
+                try {
+                    message = ircQueue.take();
+                } catch (InterruptedException e) {
+                    continue;
+                }
+
+                irc.sendMessage(message, this.channel);
+            }
+        }).start();
+
+        new Thread(() -> {
+            while (true) {
+                String line = irc.readLine();
+                if (line == null) continue;
+                if (irc.processPing(line)) continue;
+
+                IRCMessageParser.PrivateMessage privateMessage = IRCMessageParser.parsePrivateMessage(line);
+                if (privateMessage == null) continue;
+                minecraftChatQueue.add("<" + privateMessage.prefix.name + "> " + privateMessage.message);
+                System.out.println("<" + privateMessage.prefix.name + "> " + privateMessage.message);
+            }
+        }).start();
     }
 
     @Override
@@ -61,7 +107,12 @@ public class IRCChatBot implements IChatBot {
 
     @Override
     public void onMessage(String message, IChatEntity sender, IChannelBase channel) {
+        System.out.println(channel.getName());
+        if (!handler.getDefaultChannel().equals(channel)) return;
+        if (sender.isBot()) return;
+        if (sender.isServer()) return;
 
+        ircQueue.add("<" + sender.getDisplayName() + "> " + message);
     }
 
     @Override
@@ -72,5 +123,18 @@ public class IRCChatBot implements IChatBot {
     @Override
     public void onCommandMessage(String command, String[] args, String message) {
 
+    }
+
+    @Override
+    public void onTick() {
+        if (!minecraftChatQueue.isEmpty())
+        {
+            handler.sendMessage(handler.getDefaultChannel(), minecraftChatQueue.remove());
+        }
+    }
+
+    @Override
+    public String getDisplayName() {
+        return channel;
     }
 }
